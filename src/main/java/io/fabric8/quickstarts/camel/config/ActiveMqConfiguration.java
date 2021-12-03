47079553629532fa6e8e9bb3c50bd3725e8108d8
Anton Giertli <agiertli@redhat.com>
2021-12-03 17:06:51 +0100

    consumer

--- src/main/java/io/fabric8/quickstarts/camel/config/ActiveMqConfiguration.java
+++ src/main/java/io/fabric8/quickstarts/camel/config/ActiveMqConfiguration.java
@@ -7,50 +7,94 @@ import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.activemq.pool.PooledConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.xnio.Pooled;
+
+import java.util.Properties;
 
 @Configuration
 public class ActiveMqConfiguration {
 
-    @Value("${amq.url}")
-    String brokerURL;
+    @Value("${amq.producer.url}")
+    String producerBrokerURL;
 
-    @Value("${amq.username}")
-    String userName;
+    @Value("${amq.producer.username}")
+    String  producerUserName;
 
-    @Value("${amq.password}")
-    String password;
+    @Value("${amq.producer.password}")
+    String producerPassword;
+
+    @Value("${amq.consumer.url}")
+    String consumerBrokerURL;
+
+    @Value("${amq.consumer.username}")
+    String consumerUserName;
+
+    @Value("${amq.consumer.password}")
+    String consumerPassword;
 
     private static final Logger logger = LoggerFactory.getLogger(ActiveMqConfiguration.class.getName());
 
 
-//    @Bean("activemq")
-//    public ActiveMQComponent activeMq(PooledConnectionFactory pooledConnectionFactory) {
-//        ActiveMQComponent activeMqComponent = new ActiveMQComponent();
-//        activeMqComponent.setConnectionFactory(connectionFactory());
-//        activeMqComponent.setCacheLevelName("CACHE_CONSUMER");
-//
-//
-//        return activeMqComponent;
-//    }
-//
-//    @Bean
-//    public PooledConnectionFactory pooledConnectionFactory() {
-//        return new PooledConnectionFactory(connectionFactory());
-//    }
+    @Bean("activemq-consumer")
+    public ActiveMQComponent activeMqConsumer(@Qualifier("consumerpcf") PooledConnectionFactory consumerPooledConnectionFactory) {
+        ActiveMQComponent activeMqComponent = new ActiveMQComponent();
+        activeMqComponent.setConnectionFactory(consumerPooledConnectionFactory);
+        activeMqComponent.setCacheLevelName("CACHE_CONSUMER");
+        return activeMqComponent;
+    }
 
-    @Bean
-    public ActiveMQConnectionFactory myCF() {
+    @Bean("activemq-producer")
+    public ActiveMQComponent activeMq(@Qualifier("producerpcf") PooledConnectionFactory producerPooledConnectionFactory) {
+        ActiveMQComponent activeMqComponent = new ActiveMQComponent();
+        activeMqComponent.setConnectionFactory(producerPooledConnectionFactory);
+        activeMqComponent.setCacheLevelName("CACHE_CONSUMER");
+        return activeMqComponent;
+    }
 
-        logger.info("Configuring connection factory with brokerUrl {}, username {} and password ****", brokerURL, userName);
+    @Bean("producerpcf")
+    @Primary
+    public PooledConnectionFactory producerPooledConnectionFactory() {
+        PooledConnectionFactory pcf =  new PooledConnectionFactory(producerConnectionFactory());
+        Properties props = new Properties();
+        props.setProperty("maxConnections","1");
+        pcf.setProperties(props);
+        return pcf;
+    }
+
+    @Bean("consumerpcf")
+    public PooledConnectionFactory consumerPooledConnectionFactory() {
+        PooledConnectionFactory pcf =  new PooledConnectionFactory(consumerConnectionFactory());
+        Properties props = new Properties();
+        props.setProperty("maxConnections","1");
+        pcf.setProperties(props);
+        return pcf;
+    }
+
+    @Bean("producerCF")
+    public ActiveMQConnectionFactory producerConnectionFactory() {
+
+        logger.info("Configuring connection factory with brokerUrl {}, username {} and password ****", producerBrokerURL, producerUserName);
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL(brokerURL);
-        connectionFactory.setUserName(userName);
-        connectionFactory.setPassword(password);
+        connectionFactory.setBrokerURL(producerBrokerURL);
+        connectionFactory.setUserName(producerUserName);
+        connectionFactory.setPassword(producerPassword);
+        return connectionFactory;
+    }
 
 
+    @Bean("consumerCF")
+    public ActiveMQConnectionFactory consumerConnectionFactory() {
+
+        logger.info("Configuring connection factory with brokerUrl {}, username {} and password ****", consumerBrokerURL, consumerUserName);
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(consumerBrokerURL);
+        connectionFactory.setUserName(consumerUserName);
+        connectionFactory.setPassword(consumerPassword);
         return connectionFactory;
     }
 
--- src/main/java/io/fabric8/quickstarts/camel/config/OCP3AMQProducer.java
+++ src/main/java/io/fabric8/quickstarts/camel/config/OCP3AMQProducer.java
@@ -13,14 +13,18 @@ public class OCP3AMQProducer extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        //send stuff to OCP4 Broker
         from("timer:foo?fixedRate=true&period=5000").bean(this, "generateFakePerson()").to("log:info")
-               .to("activemq:queue:test?connectionFactory=#myCF");
+               .to("activemq-producer:queue:test");
+
+        // read stuff from OCP3 broker
+        from("activemq-consumer:queue:test")
+                .to("log:info");
 
     }
 
     public String generateFakePerson() {
         Faker faker = new Faker();
-
         return faker.name().fullName() + " lives on " + faker.address().streetAddress();
     }
 
